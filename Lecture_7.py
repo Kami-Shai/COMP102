7705070191864ca46e1726a60012feede7c5110e
Kami Shai <hayyanm8@gmail.com>
2025-10-06 06:32:47 +0500

    Week 4

---
+++ Lecture_7.py
@@ -0,0 +1,27 @@
+# def div_by(n,d):
+#     """
+#     docstr
+#     """
+#     if n%d == 0:
+#         return True
+#     else:
+#         return False
+# 
+# def sum_all_odd(a,b):
+#     for i in range(a,b+1):
+#         if i%2 == 1:
+#             sum+=i
+#     return i
+# 
+# def palindrome(s):
+# if s[::-1]==s:
+#     return True
+# else:
+#     return False
+# 
+# def factorial(num):
+#     f=1
+#     for i in range(1,num+1):
+#         f*=i
+#     return f
+# print(factorial(5))
